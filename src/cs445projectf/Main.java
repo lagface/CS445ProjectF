70653566d901f5433d4933997fac86368ff53a7c
Andrew Nathan Barbosa Limjoco <alimjoco@cpp.edu>
2016-10-28 14:52:28 -0700

    changed main file name

---
+++ src/cs445projectf/Main.java
@@ -0,0 +1,27 @@
+/**
+ * *************************************************************
+ * file: Main.java
+ * author: M. Lee, N. Li, A. Limjoco
+ * class: CS 445 - Computer Graphics
+ *
+ * assignment: final program
+ * date last modified: 10/28/2016
+ *
+ * purpose: Minecroft
+ *
+ ***************************************************************
+ */
+package cs445projectf;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean dreams = false;
+        while(true) {
+            if (dreams == false) {break;} 
+            System.out.println("anime is real");
+        }
+        System.out.println("buhbye dreams");
+    }
+    
+}
