f5d5bbe85711ab7bef4c28f4f7a507e5b1d20e83
Andrew Nathan Barbosa Limjoco <alimjoco@cpp.edu>
2016-10-28 15:48:06 -0700

    added createWindow() and initGL()

--- src/cs445projectf/Main.java
+++ src/cs445projectf/Main.java
@@ -13,15 +13,47 @@
  */
 package cs445projectf;
 
-public class Main {
+import java.io.*;
+import java.util.*;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.input.Keyboard;
 
-    public static void main(String[] args) {
+public class Main {
+    
+    public void start() {
         boolean dreams = false;
         while(true) {
             if (dreams == false) {break;} 
             System.out.println("anime is real");
         }
         System.out.println("buhbye dreams");
+    }
+    
+        // method: createWindow
+    // purpose: Creates the window for the environment.
+    private void createWindow() throws Exception {
+        Display.setFullscreen(false);
+        Display.setDisplayMode(new DisplayMode(640, 480));
+        Display.setTitle("Program #2");
+        Display.create();
+    }
+
+    // method: initGL
+    // purpose: Initializes the matrix for the environment.
+    private void initGL() {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-320, 320, -240, 240, 1, -1);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.start();
     }
     
 }
